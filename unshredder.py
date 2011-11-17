8e8f6b116094a8f06ed0eeda0c5e47e1c2195cf4
Arlo Carreon <arlo.carreon@gmail.com>
2011-11-17 00:32:41 -0500

    Now using Euclidean Distance Formula for distance between two single pixel columns.

--- unshredder.py
+++ unshredder.py
@@ -61,14 +61,50 @@ def getPixelColumnRGBLists(x):
 		b.append(col[2])
 	# Return list of rgb lists
 	return (r,g,b)
-	
-def printPixelColumnAverage(x):
+
+def getPixelColumnRGBAvgs(x):
 	# Get the column pixel collection
 	rgbLists = getPixelColumnRGBLists(x)
-	avgR,avgG,avgB = (sum(rgbLists[0])/imgHeight),(sum(rgbLists[1])/imgHeight),(sum(rgbLists[2])/imgHeight)
-	rgbSum = (avgR+avgG+avgB)
+	return (sum(rgbLists[0])/imgHeight),(sum(rgbLists[1])/imgHeight),(sum(rgbLists[2])/imgHeight)
+	
+
+def printPixelColumnAverage(x):
+	rgbAvgs = getPixelColumnRGBAvgs(x)
+	rgbSum = (rgbAvgs[0]+rgbAvgs[1]+rgbAvgs[2])
 	# print average rgb for column
-	print str(x)+" -> ["+str(avgR)+","+str(avgG)+","+str(avgB)+"] = "+str(rgbSum)+"/3 = "+str(rgbSum/3)
+	print str(x)+" -> ["+str(rgbAvgs[0])+","+str(rgbAvgs[1])+","+str(rgbAvgs[2])+"] = "+str(rgbSum)+"/3 = "+str(rgbSum/3)
+
+def getColumnEdges(col):
+	L_rgbAvgs = getPixelColumnRGBAvgs((col-1)*colWidth)
+	L_rgbSum = (L_rgbAvgs[0]+L_rgbAvgs[1]+L_rgbAvgs[2])
+	R_rgbAvgs = getPixelColumnRGBAvgs(col*colWidth-1)
+	R_rgbSum = (R_rgbAvgs[0]+R_rgbAvgs[1]+R_rgbAvgs[2])
+	return ( L_rgbSum/3,R_rgbSum/3 )
+
+def printColumnEdges(col):
+	edges = getColumnEdges(col)
+	print str(col)+" Left: "+str(edges[0])
+	print str(col)+" Right: "+str(edges[1])
+
+def printColumnDiff(x1,x2):
+	print str(x1)+" & "+str(x2)+" = "+str(calculate_difference(x1,x2))
+
+def calculate_difference(x1,x2):
+	from math import sqrt
+	# Get our pixel column
+	col1 = getPixelColumnLine(x1)
+	col2 = getPixelColumnLine(x2)
+	# start the diff calculation
+	sum_squared_differences = 0.0
+	pixel = 0
+	while pixel < imgHeight:
+		for channel in [0, 1, 2]:
+	            pixel_one = col1[pixel][channel]
+	            pixel_two = col2[pixel][channel]
+	            diff_squared = (pixel_one-pixel_two)*(pixel_one-pixel_two)
+	            sum_squared_differences += diff_squared
+		pixel += 1
+	return sqrt(sum_squared_differences/imgHeight)
 
 def cropColumn(col):
 	# calculate start and end points for crop of shredded image
@@ -109,4 +145,60 @@ placeColumn(6,18)
 placeColumn(1,19)
 placeColumn(10,20)
 saveUnshredded();
+ """
 """
+# This gives the pixel average for a single pixel column
+# Will be used to match up column edges
+printPixelColumnAverage(28)
+printPixelColumnAverage(29)
+printPixelColumnAverage(30)
+printPixelColumnAverage(31)
+printPixelColumnAverage(32)
+printPixelColumnAverage(33)
+printPixelColumnAverage(34)
+"""
+
+""" 
+# This will show column edges (L,R)
+# We can visually see what AI will encounter
+printColumnEdges(1)
+printColumnEdges(2)
+printColumnEdges(3)
+printColumnEdges(4)
+printColumnEdges(5)
+printColumnEdges(6)
+printColumnEdges(7)
+printColumnEdges(8)
+printColumnEdges(9)
+printColumnEdges(10)
+printColumnEdges(11)
+printColumnEdges(12)
+printColumnEdges(13)
+printColumnEdges(14)
+printColumnEdges(15)
+printColumnEdges(16)
+printColumnEdges(17)
+printColumnEdges(18)
+printColumnEdges(19)
+printColumnEdges(20)
+"""
+
+# Testing Euclidean Distance for pixel column distance
+print "Testing Edge of Column 1R (31) with 2L (32)"
+printColumnDiff(29,30)
+printColumnDiff(30,31)
+printColumnDiff(31,32)
+printColumnDiff(32,33)
+printColumnDiff(33,34)
+print "Testing Edge of Column 2R (63) with 3L (64)"
+printColumnDiff(61,62)
+printColumnDiff(62,63)
+printColumnDiff(63,64)
+printColumnDiff(64,65)
+printColumnDiff(65,66)
+print "Testing Edge of Column 3R (95) with 4L (96)"
+printColumnDiff(93,94)
+printColumnDiff(94,95)
+printColumnDiff(95,96)
+printColumnDiff(96,97)
+printColumnDiff(97,98)
