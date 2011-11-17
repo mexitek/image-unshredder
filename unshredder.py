8ba9c94b8fe08645eb390f84b967c29dfd5b65c0
Arlo Carreon <arlo.carreon@gmail.com>
2011-11-16 21:51:31 -0500

    Creates unshredded.jpg correctly, by arranging the columns manually. Needs AI.

--- unshredder.py
+++ unshredder.py
@@ -14,12 +14,16 @@ import Image
 
 # ASSUMPTIONS -----
 colWidth = 32
+shredded_filename = 'tokyo-shredded.png'
+unshredded_filename = 'unshredded.jpg'
 
 # Image data
-image = Image.open('tokyo-shredded.png')
+image = Image.open(shredded_filename)
 data = image.getdata() # This gets pixel data
 imgWidth,imgHeight = image.size
 
+# New image data
+unshredded = Image.new("RGBA", image.size)
 
 # Access an arbitrary pixel. Data is stored as a 2d array where rows are
 # sequential. Each element in the array is a RGBA tuple (red, green, blue,
@@ -35,7 +39,7 @@ def getPixelValue(x, y):
 		startx = startx + 1;
 """
 
-def getPixelColumn(x):
+def getPixelColumnLine(x):
 	# Create array that will hold column data
 	colData = []
 	# Iterate through all pixels
@@ -48,7 +52,7 @@ def getPixelColumn(x):
 
 def getPixelColumnRGBLists(x):
 	# get all pixels in the column
-	columns = getPixelColumn(x)
+	columns = getPixelColumnLine(x)
 	# Iterate and seperate into individual lists
 	r,g,b = [],[],[]
 	for col in columns:
@@ -66,15 +70,43 @@ def printPixelColumnAverage(x):
 	# print average rgb for column
 	print str(x)+" -> ["+str(avgR)+","+str(avgG)+","+str(avgB)+"] = "+str(rgbSum)+"/3 = "+str(rgbSum/3)
 
+def cropColumn(col):
+	# calculate start and end points for crop of shredded image
+	startXY = ((col-1)*colWidth, 0)
+	endXY = (col*colWidth, imgHeight)
+	# make the crop
+	return image.crop((startXY[0], startXY[1], endXY[0], endXY[1]))
+	
+def placeColumn(shredded_column, unshredded_column):
+	# Calculate destination start point for unshredded column
+	destinationXY = ((unshredded_column-1)*colWidth, 0)
+	# Put the crop in it's place
+	unshredded.paste(cropColumn(shredded_column), destinationXY)
 
-printPixelColumnAverage(26)
-printPixelColumnAverage(27)
-printPixelColumnAverage(28)
-printPixelColumnAverage(29)
-printPixelColumnAverage(30)
-printPixelColumnAverage(31)
-printPixelColumnAverage(32)
-printPixelColumnAverage(33)
-printPixelColumnAverage(34)
-printPixelColumnAverage(35)
-printPixelColumnAverage(36)
+def saveUnshredded():
+	unshredded.save(unshredded_filename, "JPEG")
+
+"""
+# This arranges the correct columns in the correct order	
+placeColumn(9,1)
+placeColumn(11,2)
+placeColumn(15,3)
+placeColumn(17,4)
+placeColumn(19,5)
+placeColumn(14,6)
+placeColumn(8,7)
+placeColumn(4,8)
+placeColumn(3,9)
+placeColumn(12,10)
+placeColumn(5,11)
+placeColumn(20,12)
+placeColumn(18,13)
+placeColumn(13,14)
+placeColumn(7,15)
+placeColumn(16,16)
+placeColumn(2,17)
+placeColumn(6,18)
+placeColumn(1,19)
+placeColumn(10,20)
+saveUnshredded();
+"""
